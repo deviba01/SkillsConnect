d0e982132d2966340e1a70d57784f5bb859b9949
harrmi02 <harrmi02@luther.edu>
2014-12-11 08:16:34 -0600

    set up with bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
+Bootstrap(app)
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
