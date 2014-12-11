e7be2175e19042c79e04a3a845583e10c9b133b5
deviba01 <deviba01@8268.dynamic.luther.edu>
2014-12-11 08:18:42 -0600

    working version

--- app/__init__.py
+++ app/__init__.py
@@ -15,3 +15,14 @@ oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
 
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
--- app/views.py
+++ app/views.py
@@ -1,27 +1,15 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, \
     login_required
+from datetime import datetime
 from app import app, db, lm, oid
 from .forms import LoginForm, EditForm
 from .models import User
-from datetime import datetime
 
 
-@app.route('/edit', methods=['GET', 'POST'])
-@login_required
-def edit():
-    form = EditForm()
-    if form.validate_on_submit():
-        g.user.nickname = form.nickname.data
-        g.user.about_me = form.about_me.data
-        db.session.add(g.user)
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('edit'))
-    else:
-        form.nickname.data = g.user.nickname
-        form.about_me.data = g.user.about_me
-    return render_template('edit.html', form=form)
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.before_request
@@ -50,21 +38,6 @@ def index():
     ]
     return render_template('index.html',
                            title='Home',
-                           user=user,
-                           posts=posts)
-
-@app.route('/user/<nickname>')
-@login_required
-def user(nickname):
-    user = User.query.filter_by(nickname=nickname).first()
-    if user == None:
-        flash('User %s not found.' % nickname)
-        return redirect(url_for('index'))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html',
                            user=user,
                            posts=posts)
 
@@ -109,3 +82,45 @@ def after_login(resp):
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user is None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm(g.user.nickname)
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
