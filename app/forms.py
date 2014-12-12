c84df709f7cee4a731b331e0d4a1c48656bd8fd7
harrmi02 <harrmi02@luther.edu>
2014-12-12 15:56:07 -0600

    new edit page and corresponding forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, Email
 from .models import User
 
 
@@ -11,7 +11,11 @@ class LoginForm(Form):
 
 class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
+    location = StringField('Location', validators=None)
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+    primaryEmail = StringField('primaryEmail', validators=[Email()])
+    learnTeach = SelectField('learnTeach', choices=[('l', 'Learn'), ('t', 'Teach')])
+    skill = StringField('skill', validators=[DataRequired()])
 
     def __init__(self, original_nickname, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
